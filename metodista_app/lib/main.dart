a1b1b00e5963903355ab30ce323f43dad7a4f206
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-02-06 20:25:36 -0300

    Creating the theme of the App

--- metodista_app/lib/main.dart
+++ metodista_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MetodistaApp());
@@ -8,9 +9,41 @@ class MetodistaApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.white,
+          secondary: Colors.black,
+        ),
+        textTheme: theme.textTheme.copyWith(
+          headline1: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          headline6: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 16,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(
+            color: Color.fromARGB(255, 245, 2, 2),
+            size: 32,
+          ),
+          titleTextStyle: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
       ),
       home: HomePage(),
     );
@@ -51,19 +84,50 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsets.zero,
           children: const <Widget>[
             DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
+              decoration: BoxDecoration(color: Colors.white),
               child: Text(
                 'Drawer Header',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Color.fromARGB(255, 245, 2, 2),
                   fontSize: 24,
                 ),
               ),
             ),
           ],
         ),
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.white,
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.all(15),
+              child: Text(
+                'Uma Família para pertencer.',
+                style: Theme.of(context).textTheme.headline6,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(60),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      backgroundColor: Colors.white,
     );
   }
 }
