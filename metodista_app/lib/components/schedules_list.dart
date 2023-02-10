0638d33c117a2117a7df58a9cabefb3ca89b07a4
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-02-10 18:00:40 -0300

    Lista de Imagens adicionada

---
+++ metodista_app/lib/components/schedules_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SchedulesList extends StatelessWidget {
+  const SchedulesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        Container(
+          height: 200,
+          color: Colors.blue,
+          child: Image.asset('assets/images/cultos.png', fit: BoxFit.cover,),
+        ),
+        Container(
+          height: 200,
+          color: Colors.blue,
+          child: Image.asset('assets/images/juventude.jpeg', fit: BoxFit.cover,),
+        ),
+        Container(
+          height: 200,
+          color: Colors.blue,
+          child: Image.asset('assets/images/kids.png', fit: BoxFit.cover,),
+        ),
+        Container(
+          height: 200,
+          color: Colors.blue,
+          child: Image.asset('assets/images/bebe-apontando.png', fit: BoxFit.cover,),
+        ),
+      ],
+      scrollDirection: Axis.vertical,
+    );
+  }
+}
