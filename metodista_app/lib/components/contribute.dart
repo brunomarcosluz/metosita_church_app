fd8a41078a93dc861438acbc0dfc759a9ad9e023
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-02-17 21:45:19 -0300

    Contribute +  Abou Us sections

--- metodista_app/lib/components/contribute.dart
+++ metodista_app/lib/components/contribute.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:url_launcher/url_launcher.dart';
 
+final Uri _urlPIX = Uri.parse('https://www.youtube.com/watch?v=6VcQDoL7DTM&ab_channel=DanielAlencar%2FAbaPai');
 class Contribute extends StatelessWidget {
   const Contribute({super.key});
 
@@ -25,25 +27,95 @@ class Contribute extends StatelessWidget {
         toolbarHeight: 85,
         elevation: 0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-               Container(
-                margin: EdgeInsets.all(30),
-                //alignment: Alignment.center,
-                color: Colors.blue,
-                height: 70,
-                child: Image.asset(
-                  'assets/images/logo-horizontal.png',
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              //color: Colors.blue,
+              height: 70,
+              margin: EdgeInsets.all(25),
+              child: Image.asset(
+                'assets/images/logo-horizontal.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+              child: Text(
+                '2 Coríntios 9:7',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
-               ),
-            ],
-          )
-        ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+              child: Text(
+                  '“Cada um dê conforme determinou em seu coração,\n não com pesar ou por obrigação, pois Deus ama quem \n dá com alegria.”',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Roboto-Italic',
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
+                  )),
+            ),
+            Container(
+              height: 200,
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/images/qrcode.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              child: Text('chave pix: pixmetodista@gmail.com',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  )),
+            ),
+            GestureDetector(
+              onTap: () => {_LaunchPIXUrl()},
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 40, 0, 15),
+                padding: EdgeInsets.all(20),
+                width: 158,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(230, 245, 5, 5),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Text(
+                  'Contribua',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+}
+
+Future<void> _LaunchPIXUrl() async {
+  if (!await launchUrl(_urlPIX)) {
+    throw Exception('Não é possível abrir $_urlPIX');
   }
 }
--- metodista_app/lib/components/drawer_list.dart
+++ metodista_app/lib/components/drawer_list.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:metodista_app/components/about_us.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:metodista_app/main.dart';
+import 'package:metodista_app/components/contribute.dart';
+//import 'package:metodista_app/components/about_us.dart';
 
 class DrawerList extends StatelessWidget {
   const DrawerList({super.key});
@@ -22,6 +25,9 @@ class DrawerList extends StatelessWidget {
             ),
           ),
           ListTile(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder:(context) => AboutUs(),));
+            },
             leading: Icon(
               Icons.info,
               color: Color.fromARGB(255, 245, 2, 2),
@@ -67,6 +73,9 @@ class DrawerList extends StatelessWidget {
             ),
           ),
           ListTile(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder:(context) => Contribute(),));
+            },
             leading: Icon(
               Icons.favorite,
               color: Color.fromARGB(255, 245, 2, 2),
