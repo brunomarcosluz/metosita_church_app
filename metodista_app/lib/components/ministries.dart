44bf6dac2b2c8eeb96ca884d068ad1fa94f2fb61
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-03-29 21:44:54 -0300

    live coding on youtube @brunomarcosluz

---
+++ metodista_app/lib/components/ministries.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class Ministries extends StatelessWidget {
+  const Ministries({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Ministérios',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        leading: BackButton(
+          color: Color.fromARGB(230, 245, 5, 5),
+        ),
+        toolbarHeight: 85,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                      height: 148,
+                      child: Image.asset(
+                        'assets/images/banner-igreja.png',
+                        fit: BoxFit.cover,
+                      )),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              child: Text(
+                'Conheça os nossos ministérios',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(230, 245, 5, 5),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Comunicação',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 50, 0, 15),
+              child: Text(
+                'Uma Família para pertencer.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
