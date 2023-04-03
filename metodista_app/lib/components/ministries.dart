7155915b16be5dfe85b413ded7dba521cc7f301c
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2023-04-03 00:01:52 -0300

    #feat- Tela Ministérios
    Finalizando a tela de ministérios - protótipo

--- metodista_app/lib/components/ministries.dart
+++ metodista_app/lib/components/ministries.dart
@@ -86,6 +86,210 @@ class Ministries extends StatelessWidget {
               ),
             ),
             Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Louvor',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Multimídia',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'KIDS',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Intercessão',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Administração',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.all(10),
+              width: 340,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(217, 245, 5, 5),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Diaconia',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget laoreet enim. Aliquam interdum enim varius porttitor egestas.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 11,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
               margin: EdgeInsets.fromLTRB(0, 50, 0, 15),
               child: Text(
                 'Uma Família para pertencer.',
